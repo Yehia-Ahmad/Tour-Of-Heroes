d84a48b2b7238cdbd0da74d41bcd25078acb4431
Yehia <Yehia>
2023-11-26 16:22:22 +0200

    UPDATE

--- src/app/Components/heroes/heroes.component.ts
+++ src/app/Components/heroes/heroes.component.ts
@@ -1,14 +1,21 @@
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from 'src/app/Heroes/mock-heroes';
 import { Hero } from 'src/app/Modules/hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
+  standalone: true,
+  imports: [FormsModule, NgIf, NgFor, UpperCasePipe],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  hero: Hero[] = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
