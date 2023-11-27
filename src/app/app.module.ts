4e9a838a0ba254c2a8da64f6f395c01df39c4fe6
Yehia <Yehia>
2023-11-27 12:40:15 +0200

    UPDATE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './Components/heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { MessagesComponent } from './Components/messages/messages.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { HeroSearchComponent } from './Components/hero-search/hero-search.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './Services/in-memory-data.service';
 
 @NgModule({
-  declarations: [AppComponent, MessagesComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HeroesComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HeroesComponent,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
 })
 export class AppModule {}
